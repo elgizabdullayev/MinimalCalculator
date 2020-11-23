ce2e30ede73382baabd5d920a638b5a5f2404ab8
student <student@students-Mac.local>
2020-11-23 03:28:55 -0800

    last

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -17,83 +17,132 @@ class ViewController: UIViewController {
     }
     
     var right = false
+    var error = false
+    var globalerror = false
+    var equals = false
+    var temp : String = ""
+
     @IBOutlet weak var Result: UILabel!
     
     @IBAction func Calculate(_ sender: UIButton) {
         
         switch (sender.tag){
         case 100:
+            if Result.text != "" {
             switch Result.text?.last!{
             case "/" , "*", "+", "-", "." :
                 right = true
-            default : right = false}
-            if Result.text! != "" && right == false
+            default : right = false} }
+            if Result.text! != "" && right == false && error == false
             {
                 Result.text = Result.text! + "+"
+                temp = temp + "+"
                 
             }
             
         case 101:
+            if Result.text != "" {
             switch Result.text?.last!{
             case "/" , "*", "+", "-", "." :
                 right = true
-            default : right = false}
-            if Result.text! != "" && right == false
+            default : right = false} }
+            if Result.text! != "" && right == false && error == false
             {
                 Result.text = Result.text! + "-"
+                temp = temp + "-"
                 
             }
            
         case 102:
+            if Result.text != "" {
             switch Result.text?.last!{
             case "/" , "*", "+", "-", "." :
                 right = true
-            default : right = false}
-            if Result.text! != "" && right == false
+            default : right = false} }
+            if Result.text! != "" && right == false && error == false
             {
                 Result.text = Result.text! + "/"
-                
+                temp = temp + "/"
             }
             
         case 104:
-           
             Result.text = ""
+            temp = ""
+            error = false
         case 103:
+            if Result.text != "" {
             switch Result.text?.last!{
             case "/" , "*", "+", "-", "." :
                 right = true
-            default : right = false}
-            if Result.text! != "" && right == false
+            default : right = false} }
+            if Result.text! != "" && right == false && error == false
             {
                 Result.text = Result.text! + "*"
+                temp = temp + "*"
                 
             }
             
         case 105:
+            if Result.text != "" {
             switch Result.text?.last!{
             case "/" , "*", "+", "-", "." :
                 right = true
-            default : right = false}
-            if Result.text! != "" && right == false
+            default : right = false} }
+            if Result.text! != "" && right == false && error == false
             {
                 Result.text = Result.text! + "."
-                
+                temp = temp + "."
             }
             
         case 107:
-            if Result.text! != ""
+            if Result.text! != "" && error == false && temp != ""
             {
-                Result.text!.removeLast()}
+                Result.text!.removeLast()
+                temp.removeLast()
+            }
         case 106:
-            let calculate = NSExpression(format: Result.text!)
-            let result = calculate.expressionValue(with: nil, context: nil) as? Double
-            Result.text = String(result!)
-        default:
-            Result.text = Result.text! + String(sender.tag)
+            
+            if temp != "" {
+            switch temp.last!{
+            case "/" , "*", "+", "-", "." :
+                equals = true
+            default : equals = false} }
             if Result.text!.contains("/0"){
-                Result.text = "Error"
+               if !Result.text!.contains("/0."){
+                    Result.text = "Error"
+                    error = true
+               }}
+            if error == false && equals == false && temp != "" {
+            temp.insert(contentsOf: "1.0*", at: temp.startIndex)
+                
+            let calculate = NSExpression(format: temp)
+            let result = calculate.expressionValue(with: nil, context: nil) as? Double
+                
+            Result.text = String(result!)
+                temp = ""
+                error = false
+            }
+            
+        default:
+    
+            if temp != ""{
+            switch temp.last!{
+            case  "+", "-" :
+            
+                temp.insert(contentsOf: "1.0*", at: temp.endIndex)
+            default: globalerror = true
+            }
+               
+            }
+            if error == false{
+                Result.text = Result.text! + String(sender.tag)
+            temp = temp + String(sender.tag)
+             
             }
         }
         
-        }
-    }
+        
+    
+}
+}
+
